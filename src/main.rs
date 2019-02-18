893672668583c86048ec9b67acd9243896f96730
Nils Martel <nils.martel@concepts-system.com>
2019-02-18 20:03:00 +0100

    Refine CLI Accepted Arguments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
 mod persistence;
 mod serialize;
 mod todo;
 
 fn main() {
-    let todos = persistence::load::<Vec<todo::Todo>>("todos").unwrap_or(Vec::new());
+    let app = App::new("Todo")
+        .version("0.1.0 alpha")
+        .author("Nils M.")
+        .about("Manages your list of things to do")
+        .subcommand(
+            SubCommand::with_name("show")
+                .about("display your todos")
+                .arg(
+                    Arg::with_name("all")
+                        .short("a")
+                        .long("all")
+                        .required(false)
+                        .help("display all todos"),
+                ),
+        )
+        .subcommand(SubCommand::with_name("set").about("check or uncheck todos"))
+        .subcommand(SubCommand::with_name("remove").about("permanently remove todos"))
+        .subcommand(SubCommand::with_name("new").about("create new todo"))
+        .get_matches();
+
+    // let todos = persistence::load::<Vec<todo::Todo>>("todos").unwrap_or(Vec::new());
 }
