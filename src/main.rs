e04da6213e83655313013790eaf61dca6a63ce21
Nils Martel <nils.martel@concepts-system.com>
2019-02-18 16:08:58 +0100

    Implement helper Methods for TODO

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,4 @@ mod persistence;
 mod serialize;
 mod todo;
 
-fn main() {
-    let todos = read_todo_file();
-}
-
-fn read_todo_file() -> Vec<todo::Todo> {
-    Vec::new()
-}
+fn main() {}
--- src/todo.rs
+++ src/todo.rs
@@ -6,6 +6,42 @@ pub struct Todo {
     description: String,
 }
 
+impl Todo {
+    pub fn new() -> Todo {
+        Todo {
+            list: String::new(),
+            done: false,
+            description: String::new(),
+        }
+    }
+
+    pub fn with_list(mut self, list: String) -> Todo {
+        self.list = list;
+        self
+    }
+
+    pub fn with_description(mut self, description: String) -> Todo {
+        self.description = description;
+        self
+    }
+
+    pub fn is_done(&self) -> bool {
+        self.done
+    }
+
+    pub fn set_done(&mut self, done: bool) {
+        self.done = done;
+    }
+
+    pub fn set_list(&mut self, list: String) {
+        self.list = list;
+    }
+
+    pub fn set_description(&mut self, description: String) {
+        self.description = description;
+    }
+}
+
 impl Serialize for Todo {
     fn deserialize(iter: &mut Iterator<Item = u8>) -> Option<Todo> {
         match (
