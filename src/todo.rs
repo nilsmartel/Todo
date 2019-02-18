c0b150ab6be0b5379c934c6abbe5c79fc6146cf8
Nils Martel <nils.martel@concepts-system.com>
2019-02-18 14:40:24 +0100

    Implement Serialize Trait for Todo Type

--- src/todo.rs
+++ src/todo.rs
@@ -7,5 +7,25 @@ pub struct Todo {
 }
 
 impl Serialize for Todo {
-    fn description(iter: &mut Iterator<Item = u8>) -> Todo {}
+    fn deserialize(iter: &mut Iterator<Item = u8>) -> Option<Todo> {
+        match (
+            String::deserialize(iter),
+            bool::deserialize(iter),
+            String::deserialize(iter),
+        ) {
+            (Some(list), Some(done), Some(description)) => Some(Todo {
+                list,
+                done,
+                description,
+            }),
+            _ => None,
+        }
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        let mut v = self.list.serialize();
+        v.append(&mut self.done.serialize());
+        v.append(&mut self.description.serialize());
+        v
+    }
 }
